9a3355f44376d2f1fdf2229413a6e24572dccbed
bennett <bennett_asia@outlook.com>
2024-08-22 11:24:38 +0800

    fix the submit tips

--- src/components/gt-captcha.vue
+++ src/components/gt-captcha.vue
@@ -2,8 +2,11 @@
 
 import type {CaptchaHandle, CaptchaProps} from "@/types/captcha.ts";
 import axios from "axios";
+import {ref} from "vue";
 
 const {config} = defineProps<CaptchaProps>();
+
+const show = ref(false);
 
 const callback: CaptchaHandle = (captchaObj: any) => {
   // 极验V3实现
@@ -17,7 +20,7 @@ const callback: CaptchaHandle = (captchaObj: any) => {
     axios.post(api, validate)
         .then(resp => {
           if (resp.data.code === 0) {
-            document.body.append("<h1>验证码已提交，可关闭本页面返回原服务了。</h1>>")
+            show.value = true;
             return;
           }
           return Promise.reject(`服务器错误: ${resp.data.message}`);
@@ -42,5 +45,5 @@ window.initGeetest(config, callback);
 </script>
 
 <template>
-  <div id="captcha-box"></div>
+  <h1 v-if="show">验证码已提交，可关闭本页面返回原服务了。</h1>
 </template>
