88d689355e116f706f9eec77d4f35b3ee52ddf89
bennett <bennett_asia@outlook.com>
2025-07-31 16:02:04 +0800

    [perf](captcha): 参数校验，无参数跳转至首页
    Signed-off-by: bennett <bennett_asia@outlook.com>

--- src/views/captcha.vue
+++ src/views/captcha.vue
@@ -2,10 +2,15 @@
 import type {CaptchaConfig} from "@/types/captcha.ts";
 import {reactive} from "vue";
 import GtCaptcha from "@/components/gt-captcha.vue";
-import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 
 const route = useRoute();
+const router = useRouter();
 const {gt, challenge, new_captcha, success} = route.query;
+// 没有 gt 和 challenge 就跳转到首页
+if (!gt || !challenge) {
+  router.push({path: "/"});
+}
 const config: CaptchaConfig = reactive({
   // 省略必须的配置参数
   gt: <string>gt,
